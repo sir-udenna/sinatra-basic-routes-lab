4d5b5111e15cae773dcbd158b9fa4e7b7bd89ba0
sir-udenna <udennaboy@gmail.com>
2021-03-15 17:44:28 -0500

    commit tree 57e2fe3d342114d3fa015b96f6ce1439a42abc0d

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        erb :name
+    end
+
+    get '/hometown' do
+        erb :hometown
+    end
+
+    get '/favorite-song' do
+        erb :favorite_song
+    end
+
 end
